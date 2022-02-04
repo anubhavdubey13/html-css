95b9be39a6f74c39789d7ecfc154abace058fb44
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2022-02-04 10:32:39 +0530

    Array Methods: Concat, Difference

--- js/LodashTonedDown/arrayMethods.js
+++ js/LodashTonedDown/arrayMethods.js
@@ -17,8 +17,35 @@ var arrayMethods = {
   compact(array){
     var filteredArr = array.filter(element => element);
     return filteredArr;
-  }
+  },
 
+  // 3. _.concat(array, [values])
+  // Creates a new array concatenating array with any additional arrays and/or values.
+  concat(array, ...args){
+    var normalArr = args;
+    var emp = array;
+    for(var i=0; i<normalArr.length; i++){
+      // probably there is a better logic out there
+      if(typeof normalArr[i] === "object"){
+        emp.push(normalArr[i][0]);
+      } else {
+        emp.push(normalArr[i]);
+      };
+    }
+    return emp;
+  },
+
+  // 4. _.difference(array, [values])
+  // Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+  difference(array, [values]){
+    var diffArr = [];
+    array.forEach(element => {
+      if(!([values].includes(element))){
+        diffArr.push(element);
+      };
+    });
+    return diffArr;
+  }
 };
 
 module.exports = arrayMethods;
--- js/LodashTonedDown/testArrayMethods.js
+++ js/LodashTonedDown/testArrayMethods.js
@@ -7,4 +7,12 @@ console.log(arrayMethods.chunk(['a', 'b', 'c', 'd'], 3));
 */
 
 // 2. Compact
-console.log(arrayMethods.compact([0, 1, false, 2, '', 3]));
+//console.log(arrayMethods.compact([0, 1, false, 2, '', 3]));
+
+// 3. Concat
+var array = [1];
+var other = arrayMethods.concat(array, 2, [3], [[4]]);
+console.log(other);
+
+// 4. Difference
+// console.log(arrayMethods.difference([2, 1], [2, 3]));
